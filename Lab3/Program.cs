4c8da62482ed682a856256fdde80b46a07ee5e8c
AntonShchur <anton2102@ukr.net>
2020-03-30 15:35:30 +0300

    LABS

---
+++ Lab3/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LAB_3
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Example1();
+
+            Dictionary<string, string> inputDictionary = new Dictionary<string, string>();
+            Dictionary<string, string> outputDictionary = new Dictionary<string, string>();
+            inputDictionary.Add("Антон", " Щ у р2 ");
+            inputDictionary.Add("А", "Щу        р");
+            ICollection<string> keys = inputDictionary.Keys;
+            List<char> charList = new List<char>();
+            List<string> stringList = new List<string>();
+            string s = "";
+            foreach (string key in keys)
+            {  
+                foreach(char element in inputDictionary[key])
+                {
+                    if(element != ' ')
+                    {
+                        charList.Add(element);
+                    }
+                    s = new string(charList.ToArray());
+                }
+                outputDictionary.Add(key, s);
+                charList.Clear();
+            }
+            Console.WriteLine(outputDictionary["А"]);
+            
+            
+        }
+        static void Example1()
+        {
+            Random rand = new Random();
+            int countOfPairs = 0;
+            List<int> listOfInt = new List<int>();
+            Console.WriteLine("Не відсортований список");
+            for (int i = 0; i < 20; i++)
+            {
+                listOfInt.Add(rand.Next(0, 20));
+                Console.Write(listOfInt[i] + "; ");
+            }
+            listOfInt.Sort();
+           // Console.WriteLine("відсортований список");
+            for (int i = 0; i < 20; i++)
+            {
+                Console.Write(listOfInt[i]+"; ");
+            }
+            for (int i = 0; i < listOfInt.Count - 1;)
+            {
+                if (listOfInt[i] == listOfInt[i + 1])
+                {
+                    countOfPairs++;
+                    i += 2;
+                }
+                else { i++; }
+            }
+            Console.WriteLine("К-сть знайдених пар: " + countOfPairs);
+
+            string DictionaryString(Dictionary<string, string> dictionary)
+            {
+                var entries = dictionary.Select(d =>
+                string.Format("\"{0}\": [{1}]", d.Key, string.Join(",", d.Value)));
+                return "{" + string.Join(",", entries) + "}";
+            }
+        }
+       
+
+
+    }
+}
---
+++ Lab4/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory directory1 = new Directory("Dir1",100);
+            Console.WriteLine("Розмір директорії: "+directory1.Size);
+            File zip_file = new File("Zip111", 10, ".zip");
+            File mp3_file = new File("mp333", 50, ".mp3");
+            File text_file = new File("text", 150, ".txt");
+            directory1.AddFile(zip_file);
+            directory1.AddFile(zip_file);//Сповіщення про наявність файлу
+            directory1.AddFile(mp3_file);//Сповіщення про переповнення
+            mp3_file.RunFile();
+            Console.WriteLine("К-сть файлів в директорії: "+directory1.CountOfFiles()); 
+
+
+        }
+        abstract class Disk 
+        {
+            public string name { get; set; }
+            private int size;
+            public int Size
+            {
+                get
+                {
+                    return size;
+                } 
+                set 
+                {
+                    if(value < 0)
+                    {
+                        Console.WriteLine("Size < 0");
+                        size = 0;
+                    }
+                    else size = value;
+                    
+                } 
+            }
+            public int fullness = 0;
+        }
+        class Directory : Disk
+        {
+            List<File> FileList = new List<File>();
+            public Directory(string DirName = "", int DirSize=0)
+            {
+                name = DirName;
+                Size = DirSize;
+            } 
+            public void AddFile(File file)
+            {
+                foreach(File f in FileList)
+                {
+                    if((f.name + f.typeOfFile) == file.name + file.typeOfFile)
+                    {
+                        Console.WriteLine("Даний файл існує");
+                    }
+                }
+                if (fullness + file.Size >= Size)
+                {
+                    Console.WriteLine("Директорія переповнена, неможливо додати файл");
+                }
+                else
+                {
+                    FileList.Add(file);
+                    fullness += file.Size;
+                    Console.WriteLine("Файл додано");
+                }    
+            }
+            public void RemoveFile(File file)
+            {
+                FileList.Remove(file);
+            }
+            public int CountOfFiles()
+            {
+                return FileList.Count;
+            }
+            public int CountOfSpecialTypeFile(string type)
+            {
+                int count = 0;
+                foreach(File file in FileList)
+                {
+                    if (file.typeOfFile == type) count++;
+                }
+                return count;
+            }
+        }
+
+        class File : Directory
+        {
+            
+            public string typeOfFile { get; set; }
+            public File(string FileName = "", int FileSize = 0, string FileType = "none")
+            {
+                name = FileName;
+                Size = FileSize;
+                typeOfFile = FileType;
+            }
+            public void RunFile()
+            {
+                Console.WriteLine("Файл запущено!!!");
+            }
+            
+        }
+        
+    }
+}
