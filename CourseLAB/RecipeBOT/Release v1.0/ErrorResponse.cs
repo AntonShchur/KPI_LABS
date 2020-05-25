97d9a250c580fdd82ea13e2d5be8e4b99a134580
AntonShchur <54263356+AntonShchur@users.noreply.github.com>
2020-05-25 16:18:18 +0300

    RecipeBOT
    REcipeBOT

---
+++ CourseLAB/RecipeBOT/Release v1.0/ErrorResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp
+{
+    public class ErrorResponse
+    {  
+        public int status { get; set; }
+        public int code { get; set; }
+        public string message { get; set; }
+        public string link { get; set; }
+       
+    }
+}
---
+++ CourseLAB/RecipeBOT/Release v1.0/FullRecipeInformation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp
+{
+    
+    public class FullRecipeInformation
+    {
+        public Extendedingredient[] extendedIngredients { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public string sourceUrl { get; set; }
+        public string image { get; set; }
+        public string summary { get; set; }
+        public string instructions { get; set; }
+        public string spoonacularSourceUrl { get; set; }
+    }
+
+    public class Extendedingredient
+    {
+        public int? id { get; set; }
+        public string aisle { get; set; } 
+        public string consistency { get; set; }
+        public string name { get; set; }
+        public string original { get; set; }
+        public string originalString { get; set; }
+        public string originalName { get; set; }
+        public float amount { get; set; }
+        public string unit { get; set; }
+    }
+}
